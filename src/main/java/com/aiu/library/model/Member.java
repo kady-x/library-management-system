2d2a9373062ed2e0a4590c251d346b891ed8213c
Basmala Samir <basmalasamir221@gmail.com>
2025-12-01 02:08:54 +0200

    Implemented full Member backend: BST-based storage ,service layer, repository wrapper, and REST controller endpoints.

--- src/main/java/com/aiu/library/model/Member.java
+++ src/main/java/com/aiu/library/model/Member.java
@@ -1,5 +1,52 @@
 package com.aiu.library.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
+    private Integer memberId;
+    private String name ;
+    private String contactInfo;
+    private String membershipDate;
+    private List<Book> borrowedBooks = new ArrayList<>();
     
+    public Integer getMemberId() {
+        return memberId;
+    }
+    
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getContactInfo() {
+        return contactInfo;
+    }
+    
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+    
+    public String getMembershipDate() {
+        return membershipDate;
+    }
+    
+    public void setMembershipDate(String membershipDate) {
+        this.membershipDate = membershipDate;
+    }
+    
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+    
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
 }
--- src/main/java/com/aiu/library/service/MemberService.java
+++ src/main/java/com/aiu/library/service/MemberService.java
@@ -1,5 +1,45 @@
 package com.aiu.library.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.aiu.library.model.Member;
+import com.aiu.library.repository.MemberRepository;
+@Service
 public class MemberService {
-    
+    private MemberRepository repo = new MemberRepository();
+
+    // Register a new member
+    public Member registerMember(Member m) {
+        if (m.getMemberId() == 0) {
+            m.setMemberId(MemberRepository.generateNextId());
+        } else if (repo.findById(m.getMemberId()) != null) {
+            throw new RuntimeException("Member ID already exists.");
+        }
+        repo.save(m);
+        return m;
+    }
+
+    // Get member by ID
+    public Member getMemberById(int id) {
+        return repo.findById(id);
+    }
+
+    // Get all members
+    public List<Member> getAllMembers() {
+        return repo.findAll();
+    }
+
+    // Update contact info of a member
+    public boolean updateContact(int id, String newContact) {
+        Member m = repo.findById(id);
+
+        if (m != null) {
+            m.setContactInfo(newContact);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
